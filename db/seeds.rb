87368fc31195a5f9d0771645c56b4aa6eed3affc
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-03-18 15:03:56 -0400

    completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,12 @@
-Student.create(name: "Melissa", grade: "10th")
-Student.create(name: "April", grade: "10th")
-Student.create(name: "Luke", grade: "9th")
-Student.create(name: "Devon", grade: "11th")
-Student.create(name: "Sarah", grade: "10th")
+Student.create(name: 'Melissa', grade: '10th')
+Student.create(name: 'April', grade: '10th')
+Student.create(name: 'Luke', grade: '9th')
+Student.create(name: 'Devon', grade: '11th')
+Student.create(name: 'Sarah', grade: '10th')
+
+namespace :db do
+  desc 'seed the database with some dummy data'
+  task seed: :environment do
+    require_relative './db/seeds'
+  end
+end
